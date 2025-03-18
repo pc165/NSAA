99cb03a73c00cf198ec2d60b7667d455dc6bfc11
chenp59 <pablo.chen@roche.com>
2025-03-18 18:02:34 +0100

    firefox middleware

--- lab2/index.js
+++ lab2/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use((req, res, next) => {
+  req.isFirefox = /Firefox/.test(req.headers["user-agent"]);
+  next();
+});
+
+app.get("/", (req, res) => {
+  const name = req.isFirefox ? "firefox user" : "world";
+  res.send(`Hello ${name}!`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
