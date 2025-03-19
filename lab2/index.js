21020b26533d8b4795628748136c9dc542e76772
chenp59 <pablo.chen@roche.com>
2025-03-19 15:33:11 +0100

    add scrypt

--- lab2/index.js
+++ lab2/index.js
@@ -7,32 +7,52 @@ const JwtStrategy = require("passport-jwt/lib/strategy");
 const { Strategy: LocalStrategy } = require("passport-local");
 const passport = require("passport");
 const app = express();
-const jwtSecret = require("crypto").randomBytes(16);
-
+const crypto = require("crypto");
+const jwtSecret = crypto.randomBytes(16);
+const scryptMcf = require("scrypt-mcf");
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
 
-db.serialize(() => {
+const insertUser = async (username, password, description) => {
+  const salt = crypto.randomBytes(16).toString("base64");
+  const hashedPassword = await scryptMcf.hash(password, {
+    saltBase64NoPadding: salt,
+  });
+
+  db.run(
+    `INSERT INTO USERS
+         VALUES (lower(hex(randomblob(16))),
+                 $username,
+                 $password,
+                 $salt,
+                 $description)`,
+    {
+      $username: username,
+      $salt: salt,
+      $password: hashedPassword,
+      $description: description,
+    },
+  );
+};
+
+db.serialize(async () => {
   db.run(`CREATE TABLE USERS
             (
-                id       UUID PRIMARY KEY,
-                username TEXT NOT NULL,
-                password TEXT NOT NULL
+                id          UUID PRIMARY KEY,
+                username    TEXT NOT NULL UNIQUE,
+                password    TEXT NOT NULL,
+                salt        TEXT NOT NULL,
+                description TEXT
             )
     `);
 
-  const stmt = db.prepare(
-    "INSERT INTO USERS VALUES (lower(hex(randomblob(16))),?,?)",
+  await insertUser(
+    "walrus",
+    "walrus",
+    "one of the users that deserve to get to this server",
   );
-  stmt.run("walrus", "walrus");
-  stmt.finalize();
-
-  db.each("SELECT * FROM USERS", (err, row) => {
-    console.log(row);
-  });
 });
 
-db.close();
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
@@ -41,24 +61,27 @@ passport.use(
   "jwtCookie",
   new JwtStrategy(
     {
-      jwtFromRequest: (req) => {
-        if (req && req.cookies) {
-          return req.cookies.jwt;
-        }
-        return null;
-      },
+      jwtFromRequest: (req) => req?.cookies?.jwt,
       secretOrKey: jwtSecret,
     },
     (jwtPayload, done) => {
-      if (jwtPayload.sub && jwtPayload.sub === "walrus") {
-        const user = {
-          username: jwtPayload.sub,
-          description: "one of the users that deserve to get to this server",
-          role: jwtPayload.role ?? "user",
-        };
-        return done(null, user);
-      }
-      return done(null, false);
+      db.get(
+        `SELECT username, description
+                 FROM USERS
+                 WHERE id = ?`,
+        [jwtPayload.sub],
+        (err, row) => {
+          if (err) {
+            return done(err, false);
+          }
+
+          return done(null, {
+            username: row.username,
+            description: row.description,
+            role: "user",
+          });
+        },
+      );
     },
   ),
 );
@@ -71,15 +94,32 @@ passport.use(
       passwordField: "password",
       session: false,
     },
-    function (username, password, done) {
-      if (username === "walrus" && password === "walrus") {
-        const user = {
-          username: "walrus",
-          description: "the only user that deserves to get to this server",
-        };
-        return done(null, user);
-      }
-      return done(null, false);
+    (username, password, done) => {
+      db.get(
+        `SELECT id, username, password
+                 FROM USERS
+                 WHERE username = ?`,
+        [username],
+        async (err, user) => {
+          if (err) {
+            return done(err);
+          }
+
+          if (!user) {
+            return done(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+
+          if (await scryptMcf.verify(password, user.password)) {
+            return done(null, user);
+          }
+
+          return done(null, false, {
+            message: "Incorrect username or password.",
+          });
+        },
+      );
     },
   ),
 );
@@ -101,7 +141,7 @@ app.post(
   }),
   (req, res) => {
     const jwtClaims = {
-      sub: req.user.username,
+      sub: req.user.id,
       iss: "localhost:3000",
       aud: "localhost:3000",
       exp: Math.floor(Date.now() / 1000) + 604800,
@@ -113,6 +153,7 @@ app.post(
     res.redirect("/");
   },
 );
+
 app.use((req, res, next) => {
   req.isFirefox = /Firefox/.test(req.headers["user-agent"]);
   next();
@@ -126,7 +167,7 @@ app.get(
   }),
   (req, res) => {
     res.send(
-      `Welcome to your private page, ${req.user.username}, you are ${req.isFirefox ? "" : "not "}using firefox!`,
+      `Welcome to your private page, ${req.user.username} ${req.user.description}, you are ${req.isFirefox ? "" : "not "}using firefox!`,
     );
   },
 );
@@ -134,10 +175,6 @@ app.get(
 app.use((req, res, next) => {
   res.status(500).send("Something broke!");
 });
-
-// app.listen(3000, () => {
-//   console.log(`start http`);
-// });
 
 https
   .createServer(
